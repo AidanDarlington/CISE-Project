5873eb6006555e116fa0a8c724f187e694087a6c
AidanDarlington <aidanlukedarlington@gmail.com>
2024-10-12 09:57:23 +1300

    Commit 1.3.1
    - Attempted Vercel Changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Enable CORS
+  app.enableCors();
+  const expressApp = express();
+
+  expressApp.use(express.json());
+
+  expressApp.get('/', (req, res) => {
+    res.send('GET request to the homepage');
+  });
+
+  expressApp.post('/', (req, res) => {
+    res.send('POST request to the homepage');
+  });
+
+  expressApp.put('/', (req, res) => {
+    res.send('PUT request to the homepage');
+  });
+
+  expressApp.patch('/', (req, res) => {
+    res.send('PATCH request to the homepage');
+  });
+
+  expressApp.delete('/', (req, res) => {
+    res.send('DELETE request to the homepage');
+  });
+
+  expressApp.options('/', (req, res) => {
+    res.send('OPTIONS request to the homepage');
+  });
+
+  app.use(expressApp);
+
   const port = process.env.PORT || 8082;
   await app.listen(port, () => console.log(`Server running on port ${port}`));
 }
