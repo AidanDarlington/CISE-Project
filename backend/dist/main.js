9d004026bbde30bad01ba53960fd2bc25f99a5c3
AidanDarlington <aidanlukedarlington@gmail.com>
2024-10-12 12:20:15 +1300

    Commit 1.3.3
    - Fixed vercel files

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -1,35 +1,10 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
-const express_1 = __importDefault(require("express"));
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors();
-    const expressApp = (0, express_1.default)();
-    expressApp.use(express_1.default.json());
-    expressApp.get('/', (req, res) => {
-        res.send('GET request to the homepage');
-    });
-    expressApp.post('/', (req, res) => {
-        res.send('POST request to the homepage');
-    });
-    expressApp.put('/', (req, res) => {
-        res.send('PUT request to the homepage');
-    });
-    expressApp.patch('/', (req, res) => {
-        res.send('PATCH request to the homepage');
-    });
-    expressApp.delete('/', (req, res) => {
-        res.send('DELETE request to the homepage');
-    });
-    expressApp.options('/', (req, res) => {
-        res.send('OPTIONS request to the homepage');
-    });
-    app.use(expressApp);
     const port = process.env.PORT || 8082;
     await app.listen(port, () => console.log(`Server running on port ${port}`));
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,24 +1,10 @@
-import { Logger } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT') || 8082;
-
-  app.enableCors({
-    origin: [
-      process.env.FRONTEND_URL,
-      /cise-project\.vercel\.app$/,
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
-
-  await app.listen(port);
-
-  Logger.log(`Application is running on http://localhost:${port}`, 'Speed');
+  app.enableCors(); // Enable CORS
+  const port = process.env.PORT || 8082;
+  await app.listen(port, () => console.log(`Server running on port ${port}`));
 }
 bootstrap();
